134ee544f2fc321b3b12f0f03ebe3a85a02f0c7f
Victor R <39545521+viicslen@users.noreply.github.com>
2026-02-10 01:06:56 -0500

    refactor: remove unnecessary imports from default.nix for cleaner configuration

--- components/ui/dms/default.nix
+++ components/ui/dms/default.nix
@@ -1,9 +1,4 @@
-{
-  imports = [
-    inputs.dms.homeModules.dank-material-shell
-    inputs.dms-plugin-registry.modules.default
-  ];
-
+{...}: {
   programs.dank-material-shell = {
     enable = true;
 
