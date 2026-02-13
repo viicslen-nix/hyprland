9d4edc5cf47f7c809e9bdb2a89755f7673e3c6cb
Victor R <39545521+viicslen@users.noreply.github.com>
2026-02-13 11:09:18 -0500

    fix: remove unnecessary window rule for flameshot in Hyprland configuration

--- components/tools/flameshot.nix
+++ components/tools/flameshot.nix
@@ -22,7 +22,6 @@ with lib; {
 
   wayland.windowManager.hyprland.settings = {
     windowrule = [
-      "match:class (flameshot), match:title (flameshot), move 0 0"
       "match:class (flameshot), match:title (flameshot), pin on"
       "match:class (flameshot), match:title (flameshot), fullscreen_state 3 3"
       "match:class (flameshot), match:title (flameshot), float on"
